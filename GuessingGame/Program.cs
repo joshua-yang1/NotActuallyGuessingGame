ede9bc5b673ef61ee84ea52a325a79b46241317f
joshuatreeee <joshuatreeee@gmail.com>
2020-01-15 11:57:23 -0500

    class challenge, not guessing game

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int costPerPerson = 13;
+            int numberOfDiners = 4;
+            int totalBill = numberOfDiners * costPerPerson;
+            Console.WriteLine(totalBill);
+            if (totalBill >= 50)
+            {
+                double newTotalBill = totalBill * .9;
+                Console.WriteLine(newTotalBill);
+                Console.ReadLine();
+            }
+            else
+            {
+                double newTotalBill = totalBill * .95;
+                Console.WriteLine(newTotalBill);
+                Console.ReadLine();
+            }
+        }
+    }
+}
